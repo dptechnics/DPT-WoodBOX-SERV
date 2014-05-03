29ca578c19e11833dba74069668ede704047b0ff
Daan Pape <daan@dptechnics.com>
2014-05-03 12:37:06 +0200

    test

--- api.c
+++ api.c
@@ -10,7 +10,58 @@
 #include "mimetypes.h"
 #include "api.h"
 
+char *response = "{\"app\" : \"DPT WoodBOX\"}";	
+
+static void write_response(struct client *cl)
+{
+	int r, copied = 0;
+	int reslen = strlen(response);
+
+	while (cl->us->w.data_bytes < 256) {
+		// Determine lenght to copy from the response buffer
+		if(sizeof(uh_buf) < reslen - copied) {
+			r = sizeof(uh_buf);
+		} else {
+			r = reslen - copied;
+		}
+		
+		/* Copy string to buffer */
+		strncpy(uh_buf, response, r);
+		
+		/* Check if all data is written */
+		if (!r) {
+			uh_request_done(cl);
+			return;
+		}
+
+		uh_chunk_write(cl, uh_buf, r);
+	}
+}
+
+static void free_response(struct client *cl)
+{
+	//TODO
+}
+
 bool handle_request(struct client *cl, char *url) {
 	printf("handling request: %s", url);
+	
+	/* write status */
+	uh_http_header(cl, 200, "OK");
+
+	ustream_printf(cl->us, "Content-Type: %s\r\n", "application/json");
+	ustream_printf(cl->us, "Content-Length: %i\r\n\r\n",strlen(response));
+
+	/* send body */
+	if (cl->request.method == UH_HTTP_MSG_HEAD) {
+		uh_request_done(cl);
+		return;
+	}
+
+	cl->dispatch.write_cb = write_response;
+	cl->dispatch.free = free_response;
+	cl->dispatch.close_fds = free_response;
+	write_response(cl);
+	
 	return true;
 }
