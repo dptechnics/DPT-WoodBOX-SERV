3daf7c87c1286c64fdf4cc824ac280fe43b581a1
dpape <daan@dptechnics.com>
2014-05-09 18:28:52 +0200

    added config.h

---
+++ config.h
@@ -0,0 +1,20 @@
+/*
+ * WoodBOX-server
+ *
+ * Server appliction for the DPTechnics WoodBOX.
+ *
+ * File: config.h
+ * Description: the woodbox server configuration header file
+ *
+ * Created by: Daan Pape
+ * Created on: May 9, 2014
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define FORK_ON_START 			false		/* True if the server should fork on startup */
+#define WORKING_BUFF_SIZE		4096		/* Size of the working buffer, should be smaller than PAGE_MAX */
+
+
+#endif
