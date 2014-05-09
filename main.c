91b6060dfb22ee4b6b8611897bd793dca48988e3
dpape <daan@dptechnics.com>
2014-05-09 18:38:45 +0200

    fixed config error

--- main.c
+++ main.c
@@ -105,7 +105,7 @@ bool load_configuration(void)
 	conf.docroot = "/www";
 
 	/* Bind a non TLS socket to port 80 */
-	if(uh_socket_bind("0.0.0.0", "80", false)) {
+	if(uh_socket_bind(NULL, "80", false)) {
 		fprintf(stderr, "[ERROR] Could not bind socket to 0.0.0.0:80\n");
 		return false;
 	}
