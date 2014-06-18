c98d6c1278bb3962fa2f0c9e9beaf43c1ce4cdc6
dpape <daan@dptechnics.com>
2014-06-18 15:28:20 +0200

    testing

--- api.c
+++ api.c
@@ -129,7 +129,8 @@ void api_handle_request(struct client *cl, char *url)
 
 	/* Search get handler */
 	handler = api_get_function(request, get_handlers, sizeof(get_handlers)/sizeof(struct f_entry));
-	printf("Size: %d\r\n", sizeof(*handlers[cl->request.method]));
+	printf("Size: %d\r\n", sizeof(handlers[cl->request.method]));
+	printf("Size: %d\r\n", sizeof(handlers[0]));
 	printf("Size: %d\r\n", sizeof(get_handlers));
 
 	/* If a handler is found execute it */
